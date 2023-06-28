1c6a3a04cc38c9ee61528245f7a2129bef246645
Renan Santos <renan.s.santos@accenture.com>
2023-06-28 18:33:28 -0300

    inclusão de dev

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,13 +1,11 @@
 export const environment = {
   firebase: {
-    projectId: 'runnerplan-prod',
-    appId: '1:829061759944:web:baf26ada31ba5e8422b6fd',
-    databaseURL: 'https://runnerplan-prod-default-rtdb.firebaseio.com',
-    storageBucket: 'runnerplan-prod.appspot.com',
-    apiKey: 'AIzaSyA56z7Uj6GqM0CDPtD0614IT_kH_cJ2nH0',
-    authDomain: 'runnerplan-prod.firebaseapp.com',
-    messagingSenderId: '829061759944',
-    measurementId: 'G-XFSYQ4847C',
+    projectId: 'runnerplan-dev',
+    appId: '1:777950203214:web:f43f2249caee174660a21f',
+    storageBucket: 'runnerplan-dev.appspot.com',
+    apiKey: 'AIzaSyAl4zJPz5N3fP-VzR_PMrUzAnUABShD3mk',
+    authDomain: 'runnerplan-dev.firebaseapp.com',
+    messagingSenderId: '777950203214',
   },
     production: false,
     apiUrl: 'https://localhost:7074/Api/v1/'
