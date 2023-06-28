36c5110837dee773c73f8b179fb9359251db6185
Renan Santos <renan.s.santos@accenture.com>
2023-06-28 17:22:36 -0300

    testes firebase

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,14 @@
 export const environment = {
+  firebase: {
+    projectId: 'runnerplan-prod',
+    appId: '1:829061759944:web:baf26ada31ba5e8422b6fd',
+    databaseURL: 'https://runnerplan-prod-default-rtdb.firebaseio.com',
+    storageBucket: 'runnerplan-prod.appspot.com',
+    apiKey: 'AIzaSyA56z7Uj6GqM0CDPtD0614IT_kH_cJ2nH0',
+    authDomain: 'runnerplan-prod.firebaseapp.com',
+    messagingSenderId: '829061759944',
+    measurementId: 'G-XFSYQ4847C',
+  },
     production: false,
     apiUrl: 'https://localhost:7074/Api/v1/'
 };
